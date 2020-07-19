92123b8d5d9779e38ebc17f5a9bb92c561721e14
ed.chang <ed.chang@xuenn.com>
2020-07-19 23:13:29 +0800

    fix login validate logic

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,12 +26,10 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-    if (to.matched.some((record) => record.meta.requiresAuth)) {
-        if (!store.state.user.authenticated) {
-            next({
-                path: '/login',
-            });
-        }
+    if (to.matched.some((record) => record.meta.requiresAuth) && !store.state.user.authenticated) {
+        next({
+            path: '/login',
+        });
     } else {
         next();
     }
