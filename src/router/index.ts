d1e47b095434fce642c70fb85d9dcf680a5edec5
ed.chang <ed.chang@xuenn.com>
2020-07-19 18:32:08 +0800

    add validate before go to chat page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,40 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import Login from '@/views/Login.vue';
+import store from '../store';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
     {
-        path: '/',
-        name: 'Home',
-        component: Home,
+        path: '/login',
+        name: 'Login',
+        component: Login,
     },
     {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+        path: '/chat',
+        name: 'Chat',
+        component: () => import(/* webpackChunkName: "chat" */ '../views/Chat.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 ];
 
 const router = new VueRouter({
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (!store.state.user.authenticated) {
+            next({
+                path: '/login',
+            });
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,12 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
+    state: {
+        user: {
+            displayName: '',
+            authenticated: false,
+        },
+    },
     mutations: {},
     actions: {},
     modules: {},
